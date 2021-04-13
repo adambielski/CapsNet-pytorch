130770c1569a15a98513d10513ecb9ef924999dc
Long <5213306+comsaint@users.noreply.github.com>
2021-04-13 19:37:47 +0800

    change in defaults

--- net.py
+++ net.py
@@ -165,8 +165,8 @@ if __name__ == '__main__':
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                        help='input batch size for testing (default: 64)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
                         help='learning rate (default: 0.001)')
